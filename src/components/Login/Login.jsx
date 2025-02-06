5bc89ccec84736cea52f6e932bf2697eb31cf3b1
Tamara Vdb <vdb.tamara@gmail.com>
2025-02-06 15:58:46 +0100

    Add toast to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,53 +2,55 @@ import { useState } from 'react';
 import { loginUser } from '../../services/Auth.service';
 import { useId } from 'react';
 import './Login.css';
+import { handleError } from '../../utilsToast';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = ({ onSwitchToRegister, onSuccessfulConnection }) => {
 
    const [username, setUsername] = useState('');
    const [password, setPassword] = useState('');
-   const [error, setError] = useState(null);
 
    const inputId = useId();
 
    const handleLoginSubmit = async (e) => {
       e.preventDefault();
-      setError(null);
 
       try {
          const success = await loginUser(username, password);
 
          if (success) {
-            console.log('Connexion reussie');
             onSuccessfulConnection();
-            window.location.reload();
+            window.location.href = '/';
          } else {
-            setError('Identifiants incorrects');
+            handleError('Identifiants incorrects');
          }
       } catch (err) {
-         setError('Une erreur est survenue');
+         handleError('Une erreur est survenue');
       }
    };
 
    return (
-      <div className="form-login">
-         <h2>Connexion</h2>
-         <p>{error}</p>
-         <form onSubmit={handleLoginSubmit}>
-            <div>
-               <label htmlFor='username'>Nom d'utilisateur</label>
-               <input id={inputId + 'username'} type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div>
-               <label htmlFor='password'>Mot de passe</label>
-               <input id={inputId + 'password'} type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
+      <>
+         <ToastContainer className="toast-container" />
+         <div className="form-login">
+            <h2>Connexion</h2>
+            <form onSubmit={handleLoginSubmit}>
+               <div>
+                  <label htmlFor='username'>Nom d'utilisateur</label>
+                  <input id={inputId + 'username'} type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+               </div>
+               <div>
+                  <label htmlFor='password'>Mot de passe</label>
+                  <input id={inputId + 'password'} type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+               </div>
 
-            <button type='submit'>Se connecter</button>
-         </form>
-         <p className="switch"
-            onClick={onSwitchToRegister}>Pas encore de compte ? <span>S'enregistrer</span></p>
-      </div>
+               <button type='submit'>Se connecter</button>
+            </form>
+            <p className="switch"
+               onClick={onSwitchToRegister}>Pas encore de compte ? <span>S'enregistrer</span></p>
+         </div>
+      </>
    );
 };
 
