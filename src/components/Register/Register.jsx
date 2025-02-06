8d6c4e6781816b94ccc9e7a8591d6d12529aba52
Tamara Vdb <vdb.tamara@gmail.com>
2025-02-06 16:27:42 +0100

    Add toast to register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,55 +2,59 @@ import { useState } from 'react';
 import { registerUser } from '../../services/Auth.service';
 import { useId } from 'react';
 import './Register.css';
+import { handleSuccess, handleError } from '../../utilsToast';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Register = ({ onSwitchToLogin, onSuccessfulConnection }) => {
+const Register = ({ onSwitchToLogin }) => {
 
    const [username, setUsername] = useState('');
    const [email, setEmail] = useState('');
    const [password, setPassword] = useState('');
-   const [error, setError] = useState(null);
 
    const inputId = useId();
 
    const handleRegisterSubmit = async (e) => {
       e.preventDefault();
-      setError(null);
 
       try {
          const success = await registerUser(username, email, password);
 
          if (success) {
-            console.log('Inscription reussie');
-            onSwitchToLogin();
+            handleSuccess('Inscription réussie');
+            setTimeout(() => onSwitchToLogin(), 2000);
          }
-      } catch (err) {
-         setError('Une erreur est survenue');
+
+      } catch (error) {
+         handleError('Erreur lors de l\'inscription');
       }
    };
 
    return (
-      <div className="form-register">
-         <h2>Inscription</h2>
-         <p>{error}</p>
-         <form onSubmit={handleRegisterSubmit}>
-            <div>
-               <label htmlFor='username'>Nom d'utilisateur</label>
-               <input id={inputId + 'username'} type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div>
-               <label htmlFor='email'>Email</label>
-               <input id={inputId + 'email'} type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div>
-               <label htmlFor='password'>Mot de passe</label>
-               <input id={inputId + 'password'} type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
+      <>
+         <ToastContainer className="toast-container" />
+         <div className="form-register">
+            <h2>Inscription</h2>
+            <form onSubmit={handleRegisterSubmit}>
+               <div>
+                  <label htmlFor='username'>Nom d'utilisateur</label>
+                  <input id={inputId + 'username'} type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+               </div>
+               <div>
+                  <label htmlFor='email'>Email</label>
+                  <input id={inputId + 'email'} type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+               </div>
+               <div>
+                  <label htmlFor='password'>Mot de passe</label>
+                  <input id={inputId + 'password'} type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+               </div>
 
-            <button type='submit'>S'inscrire</button>
-         </form>
-         <p className="switch"
-            onClick={onSwitchToLogin}>Déjà inscrit ? <span>Se connecter</span></p>
-      </div>
+               <button type='submit'>S'inscrire</button>
+            </form>
+            <p className="switch"
+               onClick={onSwitchToLogin}>Déjà inscrit ? <span>Se connecter</span></p>
+         </div>
+      </>
    );
 };
 
