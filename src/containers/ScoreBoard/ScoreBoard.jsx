46ab60f10bc2cb8dc53a9d6850ffad361533d2de
Tamara Vdb <vdb.tamara@gmail.com>
2025-02-07 15:15:41 +0100

    Start ScoreBoard

--- src/containers/ScoreBoard/ScoreBoard.jsx
+++ src/containers/ScoreBoard/ScoreBoard.jsx
@@ -1,8 +1,12 @@
 import './ScoreBoard.css';
 
 const ScoreBoard = () => {
+
    return (
       <>
+         <div className="scoreboard-container">
+            <h2>Scoreboard</h2>
+         </div>
       </>
    );
 };
