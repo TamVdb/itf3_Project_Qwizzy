2550c9688105b3a6a01646170f7e919c931363a4
Tamara Vdb <vdb.tamara@gmail.com>
2025-02-12 11:07:36 +0100

    Add global ScoreBoard component

--- src/containers/ScoreBoard/ScoreBoard.jsx
+++ src/containers/ScoreBoard/ScoreBoard.jsx
@@ -1,11 +1,17 @@
+import { useState, useEffect } from 'react';
 import './ScoreBoard.css';
 
 const ScoreBoard = () => {
+
+   const [scoreBoard, setScoreBoard] = useState([]);
 
    return (
       <>
          <div className="scoreboard-container">
             <h2>Scoreboard</h2>
+            <div className="scoreboardList">
+               <p>du texte</p>
+            </div>
          </div>
       </>
    );
