8e069366fa08aaa152e44045804910e5cb8c9c1b
Tamara <vdb.tamara@gmail.com>
2025-02-20 15:40:40 +0100

    🧼 Clean CSS

--- src/components/AddQuestion/AddQuestion.jsx
+++ src/components/AddQuestion/AddQuestion.jsx
@@ -1,51 +1,85 @@
 import { useState } from 'react';
+import { FaCirclePlus } from "react-icons/fa6";
+import { FaSave } from "react-icons/fa";
+import { handleSuccess, handleError } from '../../utilsToast';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './AddQuestion.css';
 
-const AddQuestion = () => {
+const AddQuestion = ({ questions, setQuestions, onFinish }) => {
+   const [newQuestion, setNewQuestion] = useState({
+      question: '',
+      answerFR: '',
+      answerEN: '',
+      answerAlt: ''
+   });
 
-   const [question, setQuestion] = useState('');
-   const [answerFR, setAnswerFR] = useState('');
-   const [answerEN, setAnswerEN] = useState('');
-   const [answerAlt, setAnswerAlt] = useState('');
+   const handleAddQuestion = () => {
 
-   const handleAddAllQuestionsSubmit = async (e) => {
-      e.preventDefault();
+      setQuestions([...questions, newQuestion]);
+      setNewQuestion({ question: '', answerFR: '', answerEN: '', answerAlt: '' });
 
-
+      //Toast
+      handleSuccess("Question ajoutée 👍");
    };
 
    return (
-      <>
+      <div className="add-question-section">
+         <h2>Ajouter des questions</h2>
+         <p>Ajoutez vos questions une par une, puis cliquez sur "Terminer" lorsque vous avez fini.</p>
          <div className="add-question-container">
-            <h2>Ajouter une nouvelle question</h2>
-            <form className="add-question-form" onSubmit={handleAddAllQuestionsSubmit}>
+            <div className="add-question-form">
                <div>
-                  <label htmlFor='question'>Question</label>
-                  <input type='text' id='question' value={question} onChange={(e) => setQuestion(e.target.value)} required />
+                  <label htmlFor='question'>Question<span className="required">*</span></label>
+                  <input type="text" value={newQuestion.question} onChange={(e) => setNewQuestion({ ...newQuestion, question: e.target.value })} />
+                  {newQuestion.question.trim().length > 0 && newQuestion.question.trim().length < 5 && (
+                     <p className="error">La question est requise et doit contenir au moins 5 caractères.</p>
+                  )}
                </div>
                <div>
-                  <label htmlFor='answerFR'>Réponse en français</label>
-                  <input type='text' id='answerFR' value={answerFR} onChange={(e) => setAnswerFR(e.target.value)} required />
+                  <label htmlFor='answerFR'>Réponse FR<span className="required">*</span></label>
+                  <input type="text" value={newQuestion.answerFR} onChange={(e) => setNewQuestion({ ...newQuestion, answerFR: e.target.value })} />
+                  {newQuestion.answerFR.trim().length > 0 && newQuestion.answerFR.trim().length < 5 && (
+                     <p className="error">La réponse en français est requise et doit contenir au moins 5 caractères.</p>
+                  )}
                </div>
                <div>
-                  <label htmlFor='answerEN'>Réponse en anglais</label>
-                  <input type='text' id='answerEN' value={answerEN} onChange={(e) => setAnswerEN(e.target.value)} required />
+                  <label htmlFor='answerEN'>Réponse EN</label>
+                  <input type="text" value={newQuestion.answerEN} onChange={(e) => setNewQuestion({ ...newQuestion, answerEN: e.target.value })} />
                </div>
                <div>
-                  <label htmlFor='answerAlt'>Réponse alternative</label>
-                  <input type='text' id='answerAlt' value={answerAlt} onChange={(e) => setAnswerAlt(e.target.value)} required />
+                  <label htmlFor='answerAlt'>Réponse alternative<span className="required">*</span></label>
+                  <input type="text" value={newQuestion.answerAlt} onChange={(e) => setNewQuestion({ ...newQuestion, answerAlt: e.target.value })} />
+                  {newQuestion.answerAlt.trim().length > 0 && newQuestion.answerAlt.trim().length < 5 && (
+                     <p className="error">La réponse alternative est requise et doit contenir au moins 5 caractères.</p>
+                  )}
                </div>
-               {/* Passer à la question suivante */}
-               <button onClick="">Ajouter la question</button>
 
-               {/* Envoyer toutes les questions au quiz concerné */}
-               <div>
-                  <button type="submit">Terminer</button>
-               </div>
-            </form>
+               <button type="button"
+                  onClick={handleAddQuestion}
+                  disabled={newQuestion.question.trim().length < 5 || newQuestion.answerFR.trim().length < 5 || newQuestion.answerAlt.trim().length < 5}><FaCirclePlus />Ajouter cette question</button>
+            </div>
+
+            {/* Liste des questions ajoutées */}
+            <div className="add-question-list">
+               {questions.length > 0 && (
+                  <div>
+                     <h3>Questions ajoutées :</h3>
+                     <div className="question-list">
+                        {questions.slice(-5).map((q, index) => (
+                           <div key={index} className="question">
+                              <p className="question-title">{questions.length - 5 + index + 1}. {q.question}</p>
+                              <p className="question-answers">FR: {q.answerFR} | EN: {q.answerEN} | Alt: {q.answerAlt}</p>
+                           </div>
+                        ))}
+                     </div>
+                  </div>
+               )}
+            </div>
          </div>
-      </>
+         <button className="save-quiz" type="button" onClick={onFinish}><FaSave />Terminer et enregistrer le quiz</button>
+      </div>
    );
 };
 
